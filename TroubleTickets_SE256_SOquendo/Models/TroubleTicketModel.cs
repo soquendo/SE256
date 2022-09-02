d147f6ba85a67462d28d858edd58f8741c51eebd
Oquendo <soquendo@email.neit.edu>
2022-09-02 12:37:56 -0400

    added
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   TroubleTickets_SE256_SOquendo-changes.zip
    	modified:   TroubleTickets_SE256_SOquendo/.vs/TroubleTickets_SE256_SOquendo/DesignTimeBuild/.dtbcache.v2
    	modified:   TroubleTickets_SE256_SOquendo/.vs/TroubleTickets_SE256_SOquendo/v16/.suo
    	modified:   TroubleTickets_SE256_SOquendo/Models/TroubleTicketModel.cs
    	modified:   TroubleTickets_SE256_SOquendo/Pages/AddTicket.cshtml
    	modified:   TroubleTickets_SE256_SOquendo/obj/Debug/netcoreapp3.1/TroubleTickets_SE256_SOquendo.GeneratedMSBuildEditorConfig.editorconfig
    	modified:   TroubleTickets_SE256_SOquendo/obj/Debug/netcoreapp3.1/TroubleTickets_SE256_SOquendo.assets.cache
    	modified:   TroubleTickets_SE256_SOquendo/obj/Debug/netcoreapp3.1/TroubleTickets_SE256_SOquendo.csproj.AssemblyReference.cache
    	modified:   TroubleTickets_SE256_SOquendo/obj/TroubleTickets_SE256_SOquendo.csproj.nuget.dgspec.json
    	modified:   TroubleTickets_SE256_SOquendo/obj/TroubleTickets_SE256_SOquendo.csproj.nuget.g.props
    	modified:   TroubleTickets_SE256_SOquendo/obj/project.assets.json
    	modified:   TroubleTickets_SE256_SOquendo/obj/project.nuget.cache
    
    commit -m "adds"

--- TroubleTickets_SE256_SOquendo/Models/TroubleTicketModel.cs
+++ TroubleTickets_SE256_SOquendo/Models/TroubleTicketModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,21 @@ namespace TroubleTickets_SE256_SOquendo.Models
 {
     public class TroubleTicketModel
     {
+        [Required]
         public int Ticket_ID { get; set; } //this will be a primary key, identity spec
 
+        [Required, StringLength(255)]
         public String Ticket_Title { get; set; } // basic description - 255 char
 
+        [Required]
         public String Ticket_Desc {get; set;} // more descriptive, will be text field
 
+        [Required]
+        [StringOptionsValidate(Allowed = new String[] {"Monitor", "Computer", "Printer", "Software Install", "Software Upgrade", "Configuration", "Internet"},
+            ErrorMessage ="Sorry.. category is invalid. Categories: Monitor, Computer, Printer, Software Install, Software Upgrade, Configuration, Internet")]
         public String Category { get; set; } //short string to categorize problems
 
+        [Required, EmailAddress]
         public String Reporting_Email { get; set; } //email of person reporting the ticket
 
         public DateTime Orig_Date { get; set; } //date and time the ticket was posted
@@ -26,6 +34,27 @@ namespace TroubleTickets_SE256_SOquendo.Models
         public String Responder_Email { get; set; } //email address of the responder
 
         public bool Active { get; set; } // is this active or closed (false) - binary field
+
+        [Required]
+        [Display(Name = "Original date of the problem.")]
+        [DataType(DataType.DateTime), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        [MyDate(ErrorMessage ="Future date entry not allowed")]
+        public DateTime Orig_Date { get; set; } //date and time ticket was posted
+
+        [Required]
+        [Display(Name = "Date of solutions/closure.")]
+        [DataType(DataType.DateTime), DisplayFormat(DataFormatString = "0:MM/dd/yyyy hh:mm:ss tt}", ApplyFormatInEditMode = true)]
+        [MyDate(ErrorMessage ="Future date entry not allowed")]
+        public DateTime Close_Date { get; set; } // date and time the ticket is considered closed
+
+        public String Responder_Notes { get; set; } // notes from tech support
+
+        [EmailAddress]
+        public String Responder_Email { get; set; } //email of responder
+
+        [Required]
+        public bool Active { get; set; } // active = true, closed = false
+
 
     }
 }
